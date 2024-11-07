76ca1a16de6f8be896daf16b5a0a974f58da54a1
olegbaranovskyi <oleh.baranovskyi.dev.acc@gmail.com>
2024-11-07 17:10:26 +0200

    Auto git push ...

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./profit-calc/sw.js")
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
-      });
+    navigator.serviceWorker.register("./sw.js").then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    });
   });
 }
 
